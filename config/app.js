9a5efb850cece60cf0b2a8162e015234ce0c455c
Adriano Fernandes da Silva <adrianof@lt-mbp-sp-022.ny.rga.com>
2015-06-19 14:48:16 -0300

    ...

--- config/app.js
+++ config/app.js
@@ -2,7 +2,7 @@ module.exports = {
   instagram: {
     client_id    : 'efa91b4e3a4644c29a1fd787c9e35e10',
     client_secret: '5960dabce56047179136a4f8f2951756',
-    callback_url : 'http://defqon2015.herokuapp.com//callback',
+    callback_url : 'http://defqon2015.herokuapp.com/callback',
     redirect_uri : 'http://defqon2015.herokuapp.com/'
   }
 };
