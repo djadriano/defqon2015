c99700f8a50bd34c1b21bda096860c24f616bfa6
Adriano Fernandes <djadrianof@gmail.com>
2015-07-04 14:38:39 -0300

    ...

--- controllers/instagram.js
+++ controllers/instagram.js
@@ -66,7 +66,7 @@ module.exports = {
   },
 
   getTags: function( io, socket ) {
-    io.in( socket.id ).emit('getFeedFirstTime', { show: 'https://api.instagram.com/v1/tags/hardstyle/media/recent?client_id=' + configs.instagram.client_id + '&callback=JSON_CALLBACK' });
+    io.in( socket.id ).emit('getFeedFirstTime', { show: 'https://api.instagram.com/v1/tags/qdance/media/recent?client_id=' + configs.instagram.client_id + '&callback=JSON_CALLBACK' });
   }
 
 };
