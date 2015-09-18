52b995a2730a1ab7b7a78ecf9f56546c18651b3e
Adriano Fernandes da Silva <adrianof@lt-mbp-sp-022.ny.rga.com>
2015-09-18 10:27:22 -0300

    ...

--- controllers/instagram.js
+++ controllers/instagram.js
@@ -1,54 +1,74 @@
-var configs = require( '../config/app' );
+var configs = require('../config/app');
 
 module.exports = {
 
-  Instagram: '',
+    Instagram: '',
 
-  initialize: function( instagram ) {
+    initialize: function(instagram) {
 
-    this.Instagram = instagram;
+        this.Instagram = instagram;
 
-    this.Instagram.set('client_id'    , configs.instagram.client_id);
-    this.Instagram.set('client_secret', configs.instagram.client_secret);
-    this.Instagram.set('callback_url' , configs.instagram.callback_url);
-    this.Instagram.set('redirect_uri' , configs.instagram.redirect_uri);
-    this.Instagram.set('maxSockets'   , 10);
+        this.Instagram.set('client_id', configs.instagram.client_id);
+        this.Instagram.set('client_secret', configs.instagram.client_secret);
+        this.Instagram.set('callback_url', configs.instagram.callback_url);
+        this.Instagram.set('redirect_uri', configs.instagram.redirect_uri);
+        this.Instagram.set('maxSockets', 10);
 
-    this.Instagram.subscriptions.subscribe({
-      object      : 'tag',
-      object_id   : 'ML15',
-      aspect      : 'media',
-      callback_url: configs.instagram.callback_url,
-      type        : 'subscription',
-      id          : '#'
-    });
+        this.Instagram.subscriptions.subscribe({
+            object: 'tag',
+            object_id: 'qbase',
+            aspect: 'media',
+            callback_url: configs.instagram.callback_url,
+            type: 'subscription',
+            id: '#'
+        });
 
-    this.Instagram.subscriptions.subscribe({
-      object      : 'tag',
-      object_id   : 'qdance',
-      aspect      : 'media',
-      callback_url: configs.instagram.callback_url,
-      type        : 'subscription',
-      id          : '#'
-    });
+        this.Instagram.subscriptions.subscribe({
+            object: 'tag',
+            object_id: 'qdance',
+            aspect: 'media',
+            callback_url: configs.instagram.callback_url,
+            type: 'subscription',
+            id: '#'
+        });
 
-    this.Instagram.subscriptions.subscribe({
-      object      : 'tag',
-      object_id   : 'MLNL',
-      aspect      : 'media',
-      callback_url: configs.instagram.callback_url,
-      type        : 'subscription',
-      id          : '#'
-    });
+        this.Instagram.subscriptions.subscribe({
+            object: 'tag',
+            object_id: 'hardstyle',
+            aspect: 'media',
+            callback_url: configs.instagram.callback_url,
+            type: 'subscription',
+            id: '#'
+        });
 
-  },
+        this.Instagram.subscriptions.subscribe({
+            object: 'tag',
+            object_id: 'dqaus15',
+            aspect: 'media',
+            callback_url: configs.instagram.callback_url,
+            type: 'subscription',
+            id: '#'
+        });
 
-  handshake: function( req, res ) {
-    var handshake = this.Instagram.subscriptions.handshake( req, res );
-  },
+        this.Instagram.subscriptions.subscribe({
+            object: 'tag',
+            object_id: 'dirtyworkz',
+            aspect: 'media',
+            callback_url: configs.instagram.callback_url,
+            type: 'subscription',
+            id: '#'
+        });
 
-  getTags: function( io, socket ) {
-    io.in( socket.id ).emit('getFeedFirstTime', { show: 'https://api.instagram.com/v1/tags/ML15/media/recent?client_id=' + configs.instagram.client_id + '&count=50&callback=JSON_CALLBACK' });
-  }
+    },
+
+    handshake: function(req, res) {
+        var handshake = this.Instagram.subscriptions.handshake(req, res);
+    },
+
+    getTags: function(io, socket) {
+        io.in(socket.id).emit('getFeedFirstTime', {
+            show: 'https://api.instagram.com/v1/tags/dqaus15/media/recent?client_id=' + configs.instagram.client_id + '&count=50&callback=JSON_CALLBACK'
+        });
+    }
 
 };
